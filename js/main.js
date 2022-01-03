1ac54d338d62e71be378c949b8a5b2d241123ea8
joker-0-7 <93847410+joker-0-7@users.noreply.github.com>
2022-01-03 16:07:48 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,25 +1,32 @@
-// let head = document.getElementById("head");
-// window.onscroll = () => {
-// 	head.style.backgroundColor = "#333333";
-// 	head.style.height = "60px";
-// 	head.style.transition = "0.6s";
-// };
 let mnue = document.getElementById("mnue");
 let show = document.getElementById("show");
 let span = document.getElementById("span");
 let link = document.getElementById("m-1");
 let scor = document.querySelectorAll("li a");
+let head = document.getElementById("head");
+
 show.addEventListener("click", () => {
 	mnue.style.display = "flex";
 	link.style.display = "block";
 });
-span.addEventListener("click", () => {
-	mnue.style.display = "none";
+if (document.body.scrollWidth <= 1400) {
+	mnue.style.display = "flex";
 	link.style.display = "none";
-});
-scor.forEach((e) => {
-	e.addEventListener("click", () => {
+	scor.forEach((e) => {
+		e.addEventListener("click", () => {
+			mnue.style.display = "none";
+			link.style.display = "none";
+		});
+	});
+	span.addEventListener("click", () => {
 		mnue.style.display = "none";
 		link.style.display = "none";
 	});
-});
+}
+window.onscroll = function () {
+	if (window.scrollY >= 150) {
+		head.style.backgroundColor = "#333";
+	} else {
+		head.style.backgroundColor = "transparent";
+	}
+};
